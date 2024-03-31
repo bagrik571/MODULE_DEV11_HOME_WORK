1603f39ebea3f9a7f9568cd0f355744644fae988
Bahrynovskyi <bagrik571@gmail.com>
2024-03-31 18:20:40 +0300

    added new code

--- src/main/java/org/example/ThymeleafTimeZone.java
+++ src/main/java/org/example/ThymeleafTimeZone.java
@@ -1,2 +1,80 @@
-package org.example;public class ThymeleafTimeZone {
-}
+package org.example;
+
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+
+@WebServlet("/time")
+    public class ThymeleafTimeZone extends HttpServlet {
+        private TemplateEngine engine;
+        private String currentTime;
+
+        @Override
+        public void init() throws ServletException {
+            engine = new TemplateEngine();
+
+            FileTemplateResolver resolver = new FileTemplateResolver();
+            resolver.setPrefix("/Users/bagri/java16/MODULE_DEV11_HOME_WORK/src/main/webapp/templates/");
+            resolver.setSuffix(".html");
+            resolver.setTemplateMode("HTML5");
+            resolver.setOrder(engine.getTemplateResolvers().size());
+            resolver.setCacheable(false);
+            engine.addTemplateResolver(resolver);
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.setContentType("text/html");
+
+            String timezoneParam = req.getParameter("timezone");
+            System.out.println("Received timezone parameter: " + timezoneParam);
+
+            // Відновлюємо символ "+" перед передачею до методу ZoneId.of()
+            if (timezoneParam != null) {
+                timezoneParam = timezoneParam.replace(" ", "+");
+            }
+
+//            boolean validTimeZone = timezoneParam !=null || !timezoneParam.isEmpty() || Set.of(TimeZone.getAvailableIDs()).contains(timezoneParam);
+//            if (validTimeZone){
+//                resp.addCookie(new Cookie("lastTimezone", timezoneParam));
+//            }
+            resp.addCookie(new Cookie("lastTimezone", timezoneParam));
+            Cookie[] cookies = req.getCookies();
+            if ((timezoneParam == null || timezoneParam.isEmpty()) && cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("lastTimezone".equals(cookie.getName())) {
+                        timezoneParam = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+            ZoneId zoneId = (timezoneParam != null && !timezoneParam.isEmpty()) ? ZoneId.of(timezoneParam) : ZoneId.of("UTC");
+            System.out.println("ZoneId: " + zoneId);
+
+            currentTime = ZonedDateTime.now(zoneId).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+
+            Context zoneContext = new Context(
+                    req.getLocale(),
+                    Map.of("queryParamZoneId", zoneId, "queryParamCurrentTime", currentTime)
+            );
+
+            engine.process("time", zoneContext, resp.getWriter());
+            resp.getWriter().close();
+        }
+    }
--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -1,4 +1,27 @@
 package org.example;
 
-public class TimezoneValidateFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.TimeZone;
+
+@WebFilter(value = "/time")
+public class TimezoneValidateFilter extends HttpFilter {
+    @Override
+    protected void doFilter (HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        String timezoneParam = req.getParameter("timezone");
+        boolean validTimeZone = timezoneParam !=null || !timezoneParam.isEmpty() || Set.of(TimeZone.getAvailableIDs()).contains(timezoneParam);
+        if (!validTimeZone) {
+            resp.getWriter().write("Invalid timezone");
+            resp.setStatus(400);
+            return;
+        }
+        chain.doFilter(req, resp);
+    }
 }
